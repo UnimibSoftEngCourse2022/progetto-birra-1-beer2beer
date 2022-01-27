3193de159800262acbb10af87bc2686b65c3b348
GiacoLenzo2109 <GiacoLenzo2109@gmail.com>
2022-01-27 12:11:52 +0100

    Made methods with coroutines

--- app/src/main/java/com/example/beer2beer/repository/RecipeRepository.kt
+++ app/src/main/java/com/example/beer2beer/repository/RecipeRepository.kt
@@ -1,39 +1,50 @@
 package com.example.beer2beer.repository
 
+import android.provider.ContactsContract
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.liveData
-import com.example.beer2beer.callback.Callback
+import androidx.lifecycle.map
+import androidx.room.Database
+import com.example.beer2beer.callback.Resource
 import com.example.beer2beer.database.RecipeDao
 import com.example.beer2beer.database.entities.Recipe
+import com.google.android.gms.common.api.Response
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
 
-class RecipeRepository(override val dao: RecipeDao) : IRecipeRepository {
-
-    override fun createRecipe(recipe: Recipe, callback: Callback) {
-        liveData<Recipe>(Dispatchers.IO) {
+class RecipeRepository(private val dao: RecipeDao, override val coroutineContext: CoroutineContext): CoroutineScope {
+    fun createRecipe(recipe: Recipe) {
+        launch {
             dao.insert(recipe)
-            callback.onSuccess("SUCCESS")
         }
     }
 
-    override fun updateRecipe(recipe: Recipe, callback: Callback) {
-        liveData<Recipe>(Dispatchers.IO) {
+    fun updateRecipe(recipe: Recipe) {
+        launch {
             dao.update(recipe)
-            callback.onSuccess("SUCCESS")
         }
     }
 
-    override fun deleteRecipe(recipe: Recipe, callback: Callback) {
-        liveData<Recipe>(Dispatchers.IO) {
+    fun deleteRecipe(recipe: Recipe) {
+        launch {
             dao.delete(recipe)
-            callback.onSuccess("SUCCESS")
         }
     }
 
-    override fun getAllRecipes(callback: Callback) {
-        liveData<Recipe>(Dispatchers.IO) {
-            callback.onSuccess(dao.getAll().value)
+    fun getAllRecipes() = performGetOperation(
+        databaseQuery = { dao.getAll() }
+    )
+
+    private fun <T> performGetOperation(databaseQuery: () -> LiveData<T>): LiveData<Resource<T>> =
+        liveData(Dispatchers.IO) {
+            emit(Resource.loading())
+            val source = databaseQuery.invoke().map { Resource.success(it) }
+            emitSource(source)
         }
-    }
+
 
 }
